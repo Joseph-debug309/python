cac1afd914c9760f68b51556638e9ea0029c16a6
Joseph-debug309 <joemainabz@gmail.com>
2026-02-05 01:19:43 +0300

    added

---
+++ lesson1d.py
@@ -0,0 +1,20 @@
+#create a python program that is able to calculate the simple intrest given principle as 50000, rate as 5% and time as 8 yrs. SI=(P*R*T)/100
+principle = 50000
+rate = 5
+time = 8
+simple_intrest = principle * rate * time / 100
+print("this is the simple intrest:", simple_intrest) 
+
+#Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI = (WEIGHT) / (HEIGHT SQUARED)
+weight = 78 
+height = 1.75
+BMI = weight / height ** 2
+print("This is the BMI: ", BMI)
+
+#find the area and perimeter of a rectangle whose length is 48cm and width 75cm
+length = 48
+width = 75
+area = length * width
+print("This is the area: " ,area)
+perimeter = length * 2 + width *2
+print("this is the perimeter" ,perimeter)
