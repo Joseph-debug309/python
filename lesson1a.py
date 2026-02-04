aff09d68b273f92da2e29dcb4baae785f84bc2ec
Joseph-debug309 <joemainabz@gmail.com>
2026-02-04 13:09:20 +0300

    start

---
+++ lesson1a.py
@@ -0,0 +1,25 @@
+#what is a variaable: these are lables which act as containers to hold values. in python, a variable is always associated using some values.
+#We use the assignment operator (=) to assign values to a variable.
+#anything that is inside of quotes in python is called a string
+message = "hello there hope everybody is fine?"
+print(message)
+
+message2 = " Good bye"
+print(message2)
+
+#concatenating variables (Joining two or more outputs together)
+message3 = message + message2
+print(message3)
+
+#Variabes can be named uing a letter, variable, or underscore. they can begin with a letter underscore BUT NOT a number.
+_name = "Brendah"
+print(_name)
+
+#variable names cannot contain spaces. We use the underscore to separate the names.
+total_marks = 85
+print(total_marks)
+
+#variable names are not supposed to be key words/reseved characters in python
+#e.g
+#for = nairobi
+ #   print(for)
---
+++ test.py
@@ -0,0 +1,5 @@
+print("Hello Joseph")
+
+#this is a comment
+'''this is a docstring'''
+"""this is another docstring"""
