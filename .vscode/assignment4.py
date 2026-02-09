a3e6209241343e2ffaa8b5d7979504494f090e09
Joseph-debug309 <joemainabz@gmail.com>
2026-02-09 23:39:14 +0300

    updatee

---
+++ .vscode/assignment4.py
@@ -0,0 +1,12 @@
+#2. Create a List of Colors Blue, Green, Red, Pink , Black- Using a for Loop, Loop through the
+#Colors
+colors = ["blue" , "green" , "red" , "pink" , "black"]
+for color in colors:
+    print(color)
+
+#Using a While Loop Print from 20 to 1 - 
+number = 20
+
+while number <= 20 and number >=1:
+    print(number)
+    number = number - 1
